d9aa0ebba84a3638a50162e16ebbe7bc2afbf519
marvemakuyana <marvellousmakuyana@gmail.com>
2025-01-20 17:47:45 +0200

    WIP - Auth & DB Sync, Building Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>home page content</h1>
