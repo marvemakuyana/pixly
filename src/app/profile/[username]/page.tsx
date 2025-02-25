41f6de7ded48abe98e05f84de223ead72e4a32e8
marvemakuyana <marvellousmakuyana@gmail.com>
2025-02-25 17:54:52 +0200

    Profile Page, Image Uploads with UploadThing

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,10 +1,42 @@
-import React from "react";
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
-async function Profile({ params }: { params: { username: string } }) {
-  console.log("params", params);
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
 
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Checkout ${user.username}'s profile`,
+  };
+}
+async function ProfilePageServer({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) notFound();
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  return <div>Profile</div>;
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
 }
 
-export default Profile;
+export default ProfilePageServer;
